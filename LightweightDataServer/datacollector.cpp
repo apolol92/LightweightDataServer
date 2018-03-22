99518b410a9995341464180e3253710b5e0414c3
apolol92 <apolol92@web.de>
2018-03-22 10:00:11 +0100

    Change JSON.. uses array instead of JSON-variables..

--- LightweightDataServer/datacollector.cpp
+++ LightweightDataServer/datacollector.cpp
@@ -9,12 +9,17 @@ QByteArray DataCollector::toByteArray(QList<QString> data) {
     uint time = QDateTime::currentMSecsSinceEpoch() - QDateTime(QDate::currentDate()).toMSecsSinceEpoch();
     QString json = "{\n";
     json.append("\"time\":"+QString::number(time)+",\n");
-    for(int i = 0; i < data.length(); i++) {
-        if(i<(data.length()-1)) {
-            json.append("\"val"+QString::number(i)+"\":"+data[i]+",\n");
+    for(int i = 0; i < data.length(); i++)
+    {
+        if(i==(data.length()-1)) {
+            json.append(data[i]+"]\n");
+        }
+        else if(i == 0)
+        {
+            json.append("\"values\":[" + data[i]+",");
         }
         else {
-            json.append("\"val"+QString::number(i)+"\":"+data[i]+"\n");
+            json.append(data[i]+",");
         }
     }
     json.append("}");
--- LightweightDataServer/ddatacollector.cpp
+++ LightweightDataServer/ddatacollector.cpp
@@ -9,5 +9,7 @@ QList<QString> DDataCollector::collect(void) {
     QList<QString> list;
     list.append("10");
     list.append("20.0");
+    list.append("25.0");
+    list.append("30.0");
     return list;
 }
